2cdcae79c03309cd5d7fbdbaa9fa54d1ac892c8f
Serhii Maksymenko <78811541+Serhii-Maksymenko-Projects@users.noreply.github.com>
2024-01-20 15:49:55 +0100

    configure project

---
+++ NASA/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  NASA
+//
+//  Created by    Sergey on 20.01.2024.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+}
---
+++ NASA/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  NASA
+//
+//  Created by    Sergey on 20.01.2024.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+    }
+
+}
